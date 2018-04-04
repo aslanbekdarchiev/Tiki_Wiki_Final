25f940b83a69ad017e63bb9d2355b3c2f55a5531
hikod <hikmetdogrul00@gmail.com>
2018-04-04 10:34:52 -0400

    silence web browser

--- src/test/java/com/app/pages/LoginPage.java
+++ src/test/java/com/app/pages/LoginPage.java
@@ -1,6 +1,8 @@
 package com.app.pages;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
 import com.app.utilities.Driver;
@@ -15,6 +17,23 @@ public class LoginPage {
 
 //ilyar's testing loginpage xpaths
 
+	@FindBy(xpath="//*[@id=\"page-data\"]/p[4]/a")
+	public WebElement tikiwiki;
 
+	@FindBy(xpath="//input[@name='user']")
+	public WebElement username;
+	
+	@FindBy(xpath="//input[@name='pass']")
+	public WebElement password;
+	@FindBy (xpath="//button[contains(text(), 'Log in')][@type='button']")
+	public WebElement LogInDrowDown;
+	@FindBy(xpath="//button[@class='btn btn-primary button submit']")
+	public WebElement login;
+	
+	public void login(String userName, String pwd) {
+		username.sendKeys(userName);
+		password.sendKeys(pwd);
+		login.click();
+	}
 
 }
